19d5628dbce5716cf828b9147711d226c590692a
Denis Stael <denisstael@gmail.com>
2019-11-18 00:49:34 -0300

    Screens improvements and dinamic title on the Questions screens

--- src/Interfaces/Questions.js
+++ src/Interfaces/Questions.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
 export default class Questions extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: navigation.getParam('title')
+        }
+    }
+
     render() {
-        
+        return (
+            <View>
+                <Text>Questoes</Text>
+            </View>
+        )
     }
 }
--- src/Interfaces/Subjects.js
+++ src/Interfaces/Subjects.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 export default class Subjects extends Component {
+    constructor(props) {
+        super(props)
+    }
+
     static navigationOptions = {
         title: 'Matérias'
     }
-    
+
+    screenQuestions(title) {
+        this.props.navigation.navigate("Questions", { title: title })
+    }
+
     render() {
         return (
             <View>
                 <Text>Tela 2</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        this.screenQuestions("Matemática")
+                    }}>
+                    <Text>Matemática</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => {
+                        this.screenQuestions("Português")
+                    }}>
+                    <Text>Português</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => {
+                        this.screenQuestions("História")
+                    }}>
+                    <Text>História</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => {
+                        this.screenQuestions("Geografia")
+                    }}>
+                    <Text>Geografia</Text>
+                </TouchableOpacity>
+
             </View>
         )
     }
